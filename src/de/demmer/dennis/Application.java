132126e889a244e7cb54a734803f6c39db4bb748
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-10-26 18:03:45 +0200

    initial commit

---
+++ src/de/demmer/dennis/Application.java
@@ -0,0 +1,79 @@
+package de.demmer.dennis;
+
+public class Application {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * - JavaScript - 
+		 * 
+		 * var myVar = 5;
+		 * 
+		 * var zeichenkette = "Hallo Welt";
+		 * 
+		 * var komma = 5.5;
+		 * 
+		 * 
+		 * 
+		 * - Java -
+		 * 
+		 * int myVar = 5;
+		 * 
+		 * String zeichenkette = "Hallo Welt";
+		 * 
+		 * double d = 5.5;
+		 * 
+		 * 
+		 */
+		
+		
+		
+		int myInteger;
+		myInteger = 5;
+		myInteger = 6;
+
+		//String ist eigentlich eigentlich eine Aneinanderreihung von chars
+		String greeting = "Hallo Java";
+		greeting = "Test";
+		
+		//ein char ist ein Buchstabe
+		char c = 'a';
+		
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		boolean b3 =  5 + 2 == 7;
+		boolean b4 = (5 == 5) ||  (5 - 3 == 0);
+		boolean b5 = (5 == 5) &&  (5 - 3 == 0);
+		
+		
+		/*
+		 *  ==  gleich
+		 *  !=  ungleich
+		 *  > größer
+		 *  < kleiner 
+		 *  >= größer gleich
+		 *  <= kleiner gleich
+		 *  
+		 *  
+		 *  && und
+		 *  
+		 * || oder
+		 *  
+		 *  
+		 *  
+		 */
+		
+		System.out.println(b4);
+		
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ src/de/demmer/dennis/Aufgabe2Loesung.java
@@ -0,0 +1,15 @@
+package de.demmer.dennis;
+
+public class Aufgabe2Loesung {
+
+	public static void main(String[] args) {
+		
+		String x = "Hallo";
+	
+		//hier soll "Hallo" ausgegeben werden
+		System.out.println(x);
+
+		
+	}
+	
+}
